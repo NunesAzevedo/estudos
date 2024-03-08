43963cb1585d18b44b58f11f67fcd33451f12521
Paulo Nunes <pnazevedo1@gmail.com>
2024-03-08 12:47:09 -0300

    teste de velocidade de algoritmo em c

---
+++ Estrutura de dados II/teste-velocidade.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int soma = 0;
+
+    int n = 1;
+
+    for (int i = 0; n, i++) {
+        soma = soma + i*i*i;
+    }
+
+    printf ("%d", &soma);
+
+    return 0;
+}
