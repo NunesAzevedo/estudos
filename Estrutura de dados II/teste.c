c80a55864245be4b914cfaaf6059f5fed9e53d52
Paulo Nunes <pnazevedo1@gmail.com>
2024-03-09 16:34:15 -0300

    sequencia do estudo

--- Estrutura de dados II/teste.c
+++ Estrutura de dados II/teste.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 int main() {
+    char anykey;
+
     int soma = 0;
 
     int n = 1;
 
-    for (int i = 0; n, i++) {
+    for (int i = 0; i <= n; i++) {
         soma = soma + i*i*i;
     }
 
-    printf ("%d", &soma);
+    printf ("%d", soma);
 
+    scanf("%c", &anykey);
     return 0;
 }
