9976b45793d712ace8dc7d14db593ece70a7a633
Paulo Nunes <pnazevedo1@gmail.com>
2024-03-15 13:01:50 -0300

    tentativa de rodar ex 2.3 runcodes lab icc

--- Lab ICC/RunCodes/2.3-Hora-da-experiencia.c
+++ Lab ICC/RunCodes/2.3-Hora-da-experiencia.c
@@ -2,20 +2,28 @@
 
 int main () {
     //Declaracao de variaveis
-    int
-    hora_inicial = 0,
-    minuto_inicial = 0,
-    segundo_inicial = 0,
-    duracao = 0,
+    // int
+    // hora_inicial = 0,
+    // minuto_inicial = 0,
+    // segundo_inicial = 0,
+    // duracao = 0,
 
-    hora_final = 0,
-    minuto_final = 0,
-    segundo_final = 0;
+    // hora_final = 0,
+    // minuto_final = 0,
+    // segundo_final = 0;
+
+    int
+    hora = 0,
+    minuto = 0,
+    segundo = 0,
+    duracao = 0,
+    
+    tempo = 0;
 
     //Leitura dos dados
-    scanf ("%d", &hora_inicial);
-    scanf ("%d", &minuto_inicial);
-    scanf ("%d", &segundo_inicial);
+    scanf ("%d", &hora);
+    scanf ("%d", &minuto);
+    scanf ("%d", &segundo);
     scanf ("%d", &duracao);
 
     //Calculos
@@ -25,10 +33,17 @@ int main () {
     segundo_final = segundo_inicial + (duracao - (hora_final - hora_inicial)*6060 - (minuto_final - minuto_inicial)*60 );
 */
 
-    segundo_final = segundo_inicial + duracao;
-    minuto_final = ( minuto_inicial*60 + (duracao - (segundo_final - segundo_inicial)) ) /60;
-    hora_final = hora_inicial*60*60 + duracao;
+    // segundo_final = segundo_inicial + duracao;
+    // minuto_final = ( minuto_inicial*60 + (duracao - (segundo_final - segundo_inicial)) ) /60;
+    // hora_final = hora_inicial*60*60 + duracao;
+
+    tempo = hora*3600 + minuto*60 + segundo + duracao; //Converter o tempo total em segundos
+
+    //Converter o tempo total em horas, minutos e segundos
+    hora += tempo/3600;
+    minuto += tempo/60 - tempo%3600;
+    segundo += tempo - tempo%60 - tempo%3600;
 
     //Saida
-    printf ("%d %d %d", hora_final, minuto_final, segundo_final);
+    printf ("%d %d %d", hora, minuto, segundo);
 }
