a7be4b3b6316108c506fd573a2327b74ad7d0e12
Paulo Nunes <pnazevedo1@gmail.com>
2024-04-25 01:27:11 -0300

    código do insertion sort está funcionando

---
+++ Estrutura-de-dados-II/Codigos-para-estudo/Estruturas-de-ordenacao/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void insertion_sort(int a[], int n) {
+    for (int i = 1; i < n; i++) {
+        int aux, j;
+
+        aux = a[i]; // Salva o valor que quero INSERIR no seu lugar certo
+        j = i-1;
+
+        while (j >= 0 && a[j] > aux ) { // Verifica se o número está em ordem com próximo
+            a[j+1] = a[j]; // Coloca o valor maior pra frente
+            j--;
+        }
+    a[j+1] = aux; // INSERE o valor no seu lugar correto da ordenação
+    }
+}
+
+int main () {
+    int a[4] = {7, 1, 8, 5};
+    int n = 4;
+
+    printf("\nAntes:\t");
+    for (int i = 0; i < n; i++) {
+        printf ("%d ", a[i]);
+    }    
+
+    insertion_sort(a, n);
+
+
+
+    printf("\nDepois:\t");
+    for (int i = 0; i < n; i++) {
+        printf ("%d ", a[i]);
+    }
+    printf("\n\n");
+    return 0;
+}
