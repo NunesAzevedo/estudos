d5827c39a44d6ad899e44292807f95099958832b
Paulo Nunes de Azevedo <pnazevedo@gmail.com>
2024-06-15 18:34:46 -0300

    Estrutura do codigo inicial mehorado

--- estrutura-de-dados-II/trabalho-3/main.py
+++ estrutura-de-dados-II/trabalho-3/main.py
@@ -14,11 +14,32 @@
 
 ######## Funções do programa ########
 
+def insere_T():
+    print('\nSelecione uma opcao: \n')
+    print('0: Encerra o programa')
+    print('1: Adicionar mais texto')
+    print('2: Exibe o total de palavras diferentes que foram digitadas, a palavra mais frequente e quantas vezes ela apareceu')
+    print('3: Lê um número n e em seguida n palavras, e printa n linhas, com uma palavra cada, dizendo se a palavra foi encontrada ou não, e o número de vezes que ela apareceu caso seja encontrada')
+    print('4: Lê um número n e em seguida n palavras a serem removidas do dicionário. Caso a palavra seja encontrada, deve ser removida  do dicionário e printar "palavra <palavra> foi removida", caso ela não seja encontrada, nada será feito')
+    print('5: Exibe todas as palavras contidas no dicionário e as suas respectivas posições na tabela hash')
+    print('\n')
+    T = int (input('Digite a sua opcao: '))
+    while T < 0 or T > 5:
+        print('\nOpcao inválida, insira uma opcao que exista.')
+        T = int (input('Selecione uma opcao: '))
+    return T
+
+def T_0():
+    print('\nPrograma finalizado\n')
+    exit()
+
 def T_1 (texto):
+    print ('\n')
     texto_novo = input()
     texto = texto + '\n' + texto_novo
-    print ('\n')
+    print ('\nTexto atualizado:')
     print (texto)
+    print ('\n')
     return texto
 
 def T_2 ():
@@ -37,18 +58,20 @@ def T_5 ():
 
 ######## Main do programa ########
 
+#  Insercao dos dados iniciais pelo usuario
 # S = int (input('Digite S: '))
 # C1 = int (input('Digite C1: '))
 # C2 = int (input('Digite C2: '))
 texto = (input('\nInsira o texto: \n'))
-T = int (input('\nDigite o valor de T: '))
-while T < 0 or T > 5:
-    print('\nValor de T invalido, insira um valor válido:')
-    T = int (input('Digite o valor de T: '))
+T = insere_T()
 
-# Executa o programa dependendo do valor de T inserido (Se for 0, o programa finaliza)
-while T != 0:
-    if   T == 1:
+# Executa o programa dependendo do valor de T inserido e, ao acabar,
+# solicita um novo valor para T
+while True:
+
+    if T == 0:
+        T_0()
+    elif T == 1:
         texto = T_1 (texto)
     elif T == 2:
         T_2 ()
@@ -60,9 +83,5 @@ while T != 0:
         T_5 ()
     
     # Lê um novo valor de T
-    T = int (input('\nDigite o valor de T: '))
-    while T < 0 or T > 5:
-        print('\nValor de T invalido, insira um valor válido:')
-        T = int (input('\nDigite o valor de T: '))
-    
+    T = insere_T()
 
